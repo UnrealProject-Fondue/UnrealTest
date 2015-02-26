cd73992ad707516e41f78669db1d5db929dd4bc8
Russell Tan <sell_nat@yahoo.com>
2015-02-26 15:13:27 -0800

    Added project

---
+++ PushTest/Source/PushTest/PushTestGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "PushTestGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUSHTEST_API APushTestGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
